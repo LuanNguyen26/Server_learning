e3e0ff5a29143ae7d59f23cdd34922be923d22c9
luanpro0780 <71505099+luanpro0780@users.noreply.github.com>
2021-03-23 19:34:59 +0700

    23-3

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,12 @@
+  
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  throw new Error('Lỗi rồi');
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
-router.post('/', upload.single("upload_file_input"), (req, res) => {
-  var tempPath = req.file.path;
-  //const targetPath = path.join(__dirname, "./uploads/image.png");
-  var pathSave = path.join(__dirname, '../public/images/upload/image.jpg');
-
-  console.log(tempPath);
-
-  try{
-      fs.renameSync(tempPath, pathSave);        
-  }
-  catch(err){
-      console.log(err);
-  }
-
-  res.send('send file successfull ' + pathSave);
-});
